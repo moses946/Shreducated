3881b607077f3cf7d1c30a28568e56331ab0d4d6
Moses Mwai <mosesmwaiw@gmail.com>
2024-09-06 23:15:13 +0300

    Update text on hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ export default function Hero() {
       <div className="flex flex-col gap-4">
         <p>IT&#39;S TIME TO GET</p>
         <h1 className="uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
-          Shred<span className="text-blue-400 font-medium">normous</span>
+          Shred<span className="text-blue-400 font-medium">tastic</span>
         </h1>
       </div>
       <p className="text-sm md:text-bse font-light ">
